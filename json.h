148dd9c81b3ebcddbd47f2a24f55ef832be8b4cf
ru51a4 <rruslanvaleev@yandex.ru>
2025-01-21 15:58:30 +0500

    fix

--- json.h
+++ json.h
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <fstream>
-#include <string>  
+#include <string>
 #include <map>
 #include <vector>
 #include <thread>
@@ -9,219 +9,290 @@
 
 using namespace std;
 
+namespace _json
+{
 
-namespace _json {
-
-
-
-	class token {
+	class token
+	{
 	public:
 		string value;
 		string tokenType;
-		token(string _value, string _tokenType) {
+		string valueKey;
+		token(string _value, string _tokenType, string _valueKey = "")
+		{
 			this->value = _value;
 			this->tokenType = _tokenType;
+			this->valueKey = _valueKey;
 		}
-
 	};
 
-	class node {
+	class node
+	{
 	public:
-		map <string, node*> children;
-		vector <node*> childrenArray;
-		map <string, string> values;
-		vector <string> valuesPrimitive;
+		map<string, node *> children;
+		vector<node *> childrenArray;
+		map<string, string> values;
+		vector<string> valuesPrimitive;
 		string nodeType;
-		template<typename T> T _get(string str) {
+		template <typename T>
+		T _get(string str)
+		{
 			vector<string> t;
-			if (!(str.find(".") != std::string::npos)) {
+			if (!(str.find(".") != std::string::npos))
+			{
 				t.push_back(str);
 			}
-			else {
+			else
+			{
 				_utils::utils::splitstring(str, '.', t);
 			}
-			node* curr = this;
-			for (const string item : t) {
-				if (_utils::utils::_is_number(item)) {
-					if (size(curr->valuesPrimitive) > 0) {
-						if constexpr (std::is_same_v<T, string>) {
+			node *curr = this;
+			for (const string item : t)
+			{
+				if (_utils::utils::_is_number(item))
+				{
+					if (size(curr->valuesPrimitive) > 0)
+					{
+						if constexpr (std::is_same_v<T, string>)
+						{
 							return curr->valuesPrimitive[stoi(item)];
 						}
 					}
-					else {
+					else
+					{
 						curr = curr->childrenArray[stoi(item)];
 					}
 				}
-				else {
-					if (curr->values.count(item)) {
-						if constexpr (std::is_same_v<T, string>) {
+				else
+				{
+					if (curr->values.count(item))
+					{
+						if constexpr (std::is_same_v<T, string>)
+						{
 							return curr->values[item];
 						}
 					}
-					else {
+					else
+					{
 						curr = curr->children[item];
 					}
 				}
 			}
-			if constexpr (!std::is_same_v<T, string>) {
+			if constexpr (!std::is_same_v<T, string>)
+			{
 				return curr;
 			}
 		}
 	};
 
-
-	//json
-	class json {
+	//
+	class json
+	{
 	public:
-		static node* build(string str) {
-			vector <token*> lol = json::lex(str);
+		static node *build(string str)
+		{
+			vector<token *> lol = json::lex(str);
 			return json::_build(lol);
 		}
-		static vector <token*> lex(string str) {
-			vector <token*> res;
-			string t;
-			int j = 0;
+		static vector<token *> lex(string str)
+		{
+			vector<token *> res;
 			bool isString = false;
+			string t = "";
+			auto next = [&isString, &str]()
+			{
+				while (true)
+				{
+					char ch = str.front();
+					str.erase(0, 1);
+					if (isString)
+					{
+						return ch;
+					}
+					if (ch != ' ' && ch != '\n' && ch != '\t')
+					{
+						return ch;
+					}
+				}
+			};
 
-			auto next = [&j, &isString, &str]() {
-				for (int i = j; i < size(str); i++) {
-					if (str[i] == ' ' && isString) {
-						j = i + 1;
-						return str[i];
-					}
-					if (str[i] == ' ' || str[i] == '\n' || str[i] == '\t' || str[i] == '\r') {
-						if (i == size(str) - 1) {
-							j = i + 1;
+			auto getNextChar = [&str](int i, char value, bool check = false)
+			{
+				int counter = 0;
+				while (i <= str.length() - 1)
+				{
+					if (str[i] != ' ' && str[i] != '\n' && str[i] != '\t')
+					{
+						if (str[i] == value)
+						{
+							for (int k = 0; k <= counter; k++)
+							{
+								if (!check)
+								{
+									str.erase(0, 1);
+								}
+							}
+							return true;
 						}
-						continue;
+						else
+						{
+							return false;
+						}
 					}
-					else {
-						j = i + 1;
-						return str[i];
+					else
+					{
+						i++;
+						counter++;
 					}
 				}
+				return false;
 			};
-			auto nextnext = [&j, &isString, &str](char val) {
-				for (int i = j; i < size(str); i++) {
-					if (str[i] == ' ' || str[i] == '"' || str[i] == '\n' || str[i] == '\t' || str[i] == '\r') {
-						continue;
-					}
-					else {
-						if (str[i] == val) {
-							j = i + 1;
-							return str[i];
-						}
-						else {
-							return 'x';
-						}
-					}
-				}
-			};
-			while (j <= size(str) - 1) {
+			bool isComma = false;
+			string valueKey = "";
+
+			while (str.size() > 0)
+			{
 				char cChar = next();
-				if (cChar == '\"') {
+				if (cChar == '"')
+				{
 					isString = !isString;
+				}
+				if (!isString && cChar == ':' && (!getNextChar(0, '{', true) && !getNextChar(0, '[', true)))
+				{
+					isComma = true;
+					valueKey = t;
+					t = "";
 					continue;
 				}
-				if (cChar == '{') {
-					token* _c = new token(t, string("openObj"));
+				if (cChar == '{')
+				{
+					token *_c = new token(t, string("openObj"));
 					res.push_back(_c);
 					t = "";
 				}
-				else if (cChar == ':' && nextnext('{') == '{') {
-					token* _c = new token(t, string("open"));
+				else if (cChar == ':' && getNextChar(0, '{'))
+				{
+					token *_c = new token(t, string("open"));
 					res.push_back(_c);
 					t = "";
 				}
-				else if (cChar == ':' && nextnext('[') == '[') {
-					token* _c = new token(t, string("openArray"));
+				else if (cChar == ':' && getNextChar(0, '['))
+				{
+					token *_c = new token(t, string("openArray"));
 					res.push_back(_c);
 					t = "";
 				}
-				else if (cChar == ',' || cChar == '}' || cChar == ']') {
-					if (size(t) > 0) {
-						bool isPrimitive = !(t.find(":") != std::string::npos);
-						if (isPrimitive) {
-							token* _c = new token(t, string("primitive"));
+				else if (cChar == '[')
+				{
+					token *_c = new token(t, string("openArrayPrimitive"));
+					res.push_back(_c);
+					t = "";
+				}
+				else if (!isString && (cChar == ',' || cChar == '}' || cChar == ']'))
+				{
+					if (t.size() > 0 || isComma)
+					{
+						if (isComma)
+						{
+							token *_c = new token(t, string("value"), valueKey);
 							res.push_back(_c);
+
+							isComma = false;
+							valueKey = "";
 						}
-						else {
-							token* _c = new token(t, string("value"));
+						else
+						{
+							token *_c = new token(t, string("primitive"));
 							res.push_back(_c);
 						}
 					}
-					if (cChar == '}' || cChar == ']') {
-						token* _c = new token("", string("closed"));
+					if ((cChar == '}' || cChar == ']'))
+					{
+						token *_c = new token("", string("closed"));
 						res.push_back(_c);
 					}
-					t = "";
 				}
-				else {
-					t = t + cChar;
+				else
+				{
+					if (cChar != '"')
+					{
+						t += cChar;
+					}
 				}
 			}
-			std::cout << next();
 			return res;
 		}
-		static node* _build(vector <token*> tokens) {
-			vector <node*> stack;
-			node* tObj;
+		static node *_build(vector<token *> tokens)
+		{
+			vector<node *> stack;
+			node *tObj;
 			string initToken = (*tokens.front()).tokenType;
 			tokens.erase(tokens.begin());
-			if (initToken == "openObj") {
-				node* cNode = new node();
+			if (initToken == "openObj")
+			{
+				node *cNode = new node();
 				cNode->nodeType = "object";
 				cNode->children = {};
 				stack.push_back(cNode);
 			}
-			else {
-				//todo
+			else
+			{
+				// todo
 			}
 
-			for (const token* cToken : tokens)
+			for (const token *cToken : tokens)
 			{
-				if (cToken->tokenType == "open" || cToken->tokenType == "openObj" || cToken->tokenType == "openArray") {
-					node* cNode = new node;
+				if (cToken->tokenType == "open")
+				{
+					node *cNode = new node;
 					cNode->children = {};
 					cNode->values = {};
 					tObj = stack.back();
 					stack.push_back(cNode);
-					if (cToken->tokenType == "openArray") {
-						cNode->nodeType = "array";
-					}
-					else {
-						cNode->nodeType = "object";
-					}
-					if (cToken->tokenType == "openObj") {
-						tObj->childrenArray.push_back(cNode);
-					}
-					else {
-						tObj->children[cToken->value] = cNode;
-					}
+					cNode->nodeType = "object";
+					tObj->children[cToken->value] = cNode;
 				}
-				else if (cToken->tokenType == "value") {
-					vector<string> c;
-					_utils::utils::splitstring(cToken->value, ':', c);
+				else if (cToken->tokenType == "openObj")
+				{
+
+					node *cNode = new node;
+					cNode->children = {};
+					cNode->values = {};
 					tObj = stack.back();
-					tObj->values[c.front()] = c.back();
+					stack.push_back(cNode);
+					cNode->nodeType = "object";
+					tObj->childrenArray.push_back(cNode);
 				}
-				else if (cToken->tokenType == "primitive") {
+				else if (cToken->tokenType == "value")
+				{
+					tObj = stack.back();
+					tObj->values[cToken->valueKey] = cToken->value;
+				}
+				else if (cToken->tokenType == "openArray")
+				{
+					node *cNode = new node;
+					cNode->children = {};
+					cNode->values = {};
+					tObj = stack.back();
+					stack.push_back(cNode);
+					cNode->nodeType = "array";
+					tObj->children[cToken->value] = cNode;
+				}
+				else if (cToken->tokenType == "openArrayPrimitive")
+				{
+					// todo
+				}
+				else if (cToken->tokenType == "primitive")
+				{
 					tObj = stack.back();
 					tObj->valuesPrimitive.push_back(cToken->value);
 				}
-				else if (cToken->tokenType == "closed" && size(stack) > 1) {
+				else if (cToken->tokenType == "closed" && size(stack) > 1)
+				{
 					stack.pop_back();
 				}
 			}
 			return stack.front();
-
 		}
-
 	};
-
-
-	//
-
-
-
 }
